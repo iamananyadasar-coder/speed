a03f61973d2bc141f6d87db7c606a3c99fb843e1
Ananya.Dasar <01fe24ica011@kletech.ac.in>
2025-11-27 23:14:09 +0530

    modified

--- speed.py
+++ speed.py
@@ -4,11 +4,12 @@ def calculate_speed(distance, time):
     return distance / time
 
 if _name_ == "_main_":
-
+    # If command line arguments are given
     if len(sys.argv) == 3:
         distance = float(sys.argv[1])
         time = float(sys.argv[2])
     else:
+        # Else take user input
         distance = float(input("Enter distance (km): "))
         time = float(input("Enter time (hours): "))
 
